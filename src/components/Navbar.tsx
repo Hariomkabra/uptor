dc3b9c7a6b95fbd140364529cf79b9f855877698
Hariom Kabra <143120733+Hariomkabra@users.noreply.github.com>
2025-04-08 13:05:45 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,12 @@
-
-
 const Navbar = () => {
   return (
-    <nav className="bg-white shadow-sm fixed w-full z-50">
+    <nav className="bg-black shadow-sm fixed w-full z-50 border-b border-yellow-500">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-center h-16">
           <div className="flex items-center">
             <span className="text-2xl font-bold">
-              <span className="text-pink-600">U</span>
-              <span className="text-black-500">ptor</span>
+              <span className="text-yellow-400">U</span>
+              <span className="text-white">ptor</span>
             </span>
           </div>
         </div>
